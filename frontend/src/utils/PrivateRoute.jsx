af09521e54860a3cecc03034a53203c6e37af8ba
Bagus Nararya Nanda Raditya <87893990+nararyacodetechy@users.noreply.github.com>
2024-04-15 18:33:06 +0800

    Add Save Cookies Authentication

--- frontend/src/utils/PrivateRoute.jsx
+++ frontend/src/utils/PrivateRoute.jsx
@@ -1,9 +1,10 @@
 import PropTypes from 'prop-types';
 import { Navigate } from 'react-router-dom';
+import { getToken, saveToken } from './TokenUtilities';
 
 const PrivateRoute = ({ element }) => {
-  const token = localStorage.getItem('token');
-  const isUserLoggedIn = !!token;
+  const token = getToken();
+  const isUserLoggedIn = !!token && !saveToken();
 
   return isUserLoggedIn ? element : <Navigate to="/auth/sign-in" replace />;
 };
