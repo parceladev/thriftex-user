c33c2dd96b0e74a43428074263d68ca4a7419428
Bagus Nararya Nanda Raditya <87893990+nararyacodetechy@users.noreply.github.com>
2024-05-03 23:52:31 +0800

    Add button legit for Mobile

--- frontend/src/components/legitchecks/ButtonFormLegit.jsx
+++ frontend/src/components/legitchecks/ButtonFormLegit.jsx
@@ -4,14 +4,13 @@ const ButtonFormLegit = () => {
   const isUserLoggedIn = !!getAccessToken();
 
   return (
-    <div
-      className={`bg-black hidden sm:flex dark:bg-white border border-black px-5`}
-    >
-      <div className="flex justify-center min-w-[150px]">
+    <>
+      {/* Desktop button */}
+      <div className="hidden sm:flex bg-black dark:bg-white border border-black px-5 justify-center min-w-[175px]">
         {isUserLoggedIn ? (
           <a
             href="/user/legit-check-form"
-            className={`text-white dark:text-black flex gap-3 items-center`}
+            className="flex items-center gap-3 text-white dark:text-black"
           >
             <p className="text-xl uppercase">Legit Check</p>
             <p className="text-4xl">+</p>
@@ -19,14 +18,32 @@ const ButtonFormLegit = () => {
         ) : (
           <a
             href="/auth/sign-in"
-            className={`text-white dark:text-black flex gap-3 items-center`}
+            className="flex items-center gap-3 text-white dark:text-black"
           >
             <p className="text-xl uppercase">Legit Check</p>
             <p className="text-4xl">+</p>
           </a>
         )}
       </div>
-    </div>
+      {/* Mobile floating button */}
+      <div className="fixed flex items-center justify-center w-24 h-24 p-3 bg-black border border-black rounded-full shadow-lg sm:hidden bottom-8 right-8 dark:bg-white">
+        {isUserLoggedIn ? (
+          <a
+            href="/user/legit-check-form"
+            className="flex items-center justify-center text-white dark:text-black"
+          >
+            <p className="text-6xl">+</p>
+          </a>
+        ) : (
+          <a
+            href="/auth/sign-in"
+            className="flex items-center justify-center text-white dark:text-black"
+          >
+            <p className="text-6xl">+</p>
+          </a>
+        )}
+      </div>
+    </>
   );
 };
 
