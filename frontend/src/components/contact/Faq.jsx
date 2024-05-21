5b89fecd60c74a24f5ee8637f0384d653688f893
DewakSuputra <ikmg.basudewa.s01@gmail.com>
2024-05-21 11:05:27 +0800

    Add animation FAQ

--- frontend/src/components/contact/Faq.jsx
+++ frontend/src/components/contact/Faq.jsx
@@ -1,9 +1,11 @@
 import { FiChevronDown, FiChevronUp } from 'react-icons/fi';
-import { useState } from 'react';
+import { useState,useEffect, useRef } from 'react';
 import { useTranslation } from "react-i18next";
 
 // eslint-disable-next-line react/prop-types
 const FAQItem = ({ question, answer, isActive, setActive }) => {
+  const contentRef = useRef(null);
+
   return (
     <>
       <button
@@ -13,11 +15,17 @@ const FAQItem = ({ question, answer, isActive, setActive }) => {
         <h3 className="pr-16 font-semibold">{question}</h3>
         {isActive ? <FiChevronUp /> : <FiChevronDown />}
       </button>
-      {isActive && (
+      <div
+        ref={contentRef}
+        className={`transition-all duration-500 ease-in-out overflow-hidden`}
+        style={{
+          maxHeight: isActive ? `${contentRef.current.scrollHeight}px` : '0px',
+        }}
+      >
         <div className="pb-4">
           <p className="font-sans">{answer}</p>
         </div>
-      )}
+      </div>
       <hr />
     </>
   );
@@ -26,10 +34,18 @@ const FAQItem = ({ question, answer, isActive, setActive }) => {
 const Faq = () => {
   const { t } = useTranslation();
   const [activeIndex, setActiveIndex] = useState(null);
+  const [isVisible, setIsVisible] = useState(false);
 
   const toggleActiveIndex = (index) => {
     setActiveIndex(index === activeIndex ? null : index);
   };
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsVisible(true);
+    }, 500); // Menambahkan sedikit penundaan sebelum mengatur isVisible ke true
+  }, []);
+
 
   const faqData = [
     {
@@ -70,8 +86,8 @@ const Faq = () => {
   ];
 
   return (
-    <div className="flex flex-col w-full md:flex-1">
-      <h2 className="mb-6 text-xl font-semibold">FREQUENTLY ASKED QUESTIONS</h2>
+    <div className={`flex flex-col w-full md:flex-1 transition-opacity duration-1000 ease-in-out ${isVisible ? 'opacity-100' : 'opacity-0'}`}>
+      <h2 className="mb-6 text-xl font-semibold uppercase">Frequently Asked Questions</h2>
       {faqData.map((faq, index) => (
         <FAQItem
           key={index}
